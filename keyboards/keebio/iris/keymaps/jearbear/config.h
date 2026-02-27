5e7d7238ec8acc7c20c50c53e8b30484b81bea81
Jerry Tegno <jerrytegno@gmail.com>
2026-02-27 10:23:57 -0800

    Save

---
+++ keyboards/keebio/iris/keymaps/jearbear/config.h
@@ -0,0 +1,12 @@
+// Make it such that dual-function keys will always trigger the hold action if
+// any other key is also pressed at the same time.
+#define HOLD_ON_OTHER_KEY_PRESS
+
+#define RGBLIGHT_SLEEP
+
+#define TAPPING_TERM 200
+
+#define QUICK_TAP_TERM 0
+
+
+
